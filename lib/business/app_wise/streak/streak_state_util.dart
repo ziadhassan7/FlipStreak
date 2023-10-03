b118e9d42c5ba8399246224d7f90404c5ea7c823
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-03 17:36:13 +0300

    Document Functions

--- lib/business/app_wise/streak/streak_state_util.dart
+++ lib/business/app_wise/streak/streak_state_util.dart
@@ -12,16 +12,20 @@ class StreakStateUtil {
     DateTime lastSavedDate = _hive.getLastDate();
     //Durations
     Duration remaining = currentDate.difference(lastSavedDate).abs();
+    //Countdown Gap
     Duration hoursMin = const Duration(hours: 24);
     Duration hoursMax = const Duration(hours: 56);
 
 
+    /// Same State (didn't enter Countdown Gap)                                 -- Don't update date
     if(remaining < hoursMin) {
       _hive.updateStreakState(SAME_DATE_STATE);
 
+    /// Countdown (entered Countdown Gap)                                       -- You can change streak
     } else if (remaining > hoursMin  &&  remaining < hoursMax){
       _hive.updateStreakState(COUNTDOWN_STATE);
 
+    /// Ended (Reset)                                                           -- Streak is back to 0
     }else if (remaining > hoursMax){
       _hive.updateStreakState(ENDED_STATE);
       resetAllCounter();

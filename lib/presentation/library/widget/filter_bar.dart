da5a2cc5d74f31d58c23eca7604f3b33f1ce5362
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-04 01:26:54 +0300

    Add search bar

--- lib/presentation/library/widget/filter_bar.dart
+++ lib/presentation/library/widget/filter_bar.dart
@@ -15,35 +15,53 @@ class FilterBar extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
 
-      child: InkWell(
-        onTap: (){
-          CategoryMenu(context, currentCategory);
-        },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+        decoration: BoxDecoration(
+          color: colorAccent.withOpacity(0.04),
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
 
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 18),
-          decoration: BoxDecoration(
-            color: colorAccent.withOpacity(0.04),
-            borderRadius: const BorderRadius.all(Radius.circular(20)),
-          ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 6,
+              child: TextFormField(
+                style: const TextStyle(color: colorBrown),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search_rounded, color: colorAccent,),
+                  border: InputBorder.none,
+                  hintText: 'Search your books',
+                  hintStyle: TextStyle(color: colorAccent.withOpacity(0.2)),
+                ),
+              ),
+            ),
 
-          child: Row(
-            children: [
-              TextInriaSans("Category:", size: 16, color: Colors.black38,),
+            const Spacer(),
 
-              const Spacer(),
+            InkWell(
+              onTap: (){
+                CategoryMenu(context, currentCategory);
+              },
 
-              TextInriaSans(
-                currentCategory,
-                size: 16,
-                color: colorAccent,
-                weight: FontWeight.bold,),
+              child: Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    TextInriaSans(
+                      currentCategory,
+                      size: 16,
+                      color: colorAccent,
+                      weight: FontWeight.bold,),
 
-              const SizedBox(width: 10,),
+                    const SizedBox(width: 10,),
 
-              const Icon(Icons.arrow_drop_down_rounded, color: Colors.black87,),
-            ],
-          ),
+                    const Icon(Icons.arrow_drop_down_rounded, color: Colors.black87,),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

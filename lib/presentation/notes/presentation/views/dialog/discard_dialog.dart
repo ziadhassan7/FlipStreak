138b626a36116772ff0ecad6aee78d1952cde55f
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-12 06:38:13 +0300

    "Discard" -> "Delete"

--- lib/presentation/notes/presentation/views/dialog/discard_dialog.dart
+++ lib/presentation/notes/presentation/views/dialog/discard_dialog.dart
@@ -13,7 +13,7 @@ class DiscardDialog {
 
     DialogWidget(
         context,
-        dominantButtonTitle: "Discard",
+        dominantButtonTitle: "Delete",
         dominantButtonColor: colorOrange,
 
         dominantButtonFunction: (){

c629b40affc327127b52a7cedf71a5bf8fc24029
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-12 05:12:54 +0300

    moved to global folder

--- lib/presentation/notes/presentation/views/empty_notes_widget.dart
+++ lib/presentation/notes/presentation/views/empty_notes_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-import '../../../../../../app_constants/color_constants.dart';
-import '../../../../../../business/route_util.dart';
-import '../../../../../views/text_inria_sans.dart';
-import '../../note_edit/screen/note_edit.dart';
+import '../../../../app_constants/color_constants.dart';
+import '../../../../business/route_util.dart';
+import '../../../views/text_inria_sans.dart';
+import '../pages/note_edit/screen/note_edit.dart';
 
 class EmptyNotesWidget extends ConsumerWidget {
   const EmptyNotesWidget({Key? key}) : super(key: key);

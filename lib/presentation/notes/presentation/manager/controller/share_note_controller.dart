573b029555323ef825dd5432acced1bcb14e6ee1
ziadhassan7 <ziadhassan7@gmail.com>
2023-10-13 22:59:21 +0300

    adjust resolution for sharing big text

--- lib/presentation/notes/presentation/manager/controller/share_note_controller.dart
+++ lib/presentation/notes/presentation/manager/controller/share_note_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flip_streak/business/share_util.dart';
+import 'package:flip_streak/presentation/notes/presentation/manager/controller/note_controller.dart';
 import 'package:flip_streak/presentation/notes/presentation/pages/note_share/view/share_item.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:ui' as ui;
@@ -12,12 +13,21 @@ class NoteShareController {
 
   static Future<Uint8List> _getSharedImage() async {
     RenderRepaintBoundary boundary = ShareItem.shareWidgetKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
-    ui.Image image = await boundary.toImage(pixelRatio: 4.5); //pixelRatio for quality
+    ui.Image image = await boundary.toImage(pixelRatio: _getProperResolution()); //pixelRatio for quality
     ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
     Uint8List pngBytes = byteData!.buffer.asUint8List();
 
     return pngBytes;
   }
 
+  static double _getProperResolution(){
+    //if note length is too big
+    if(NoteController.noteBody.text.length > 200){
+      return 8; //big widget
+
+    } else {
+      return 4.5;//small widget
+    }
+  }
 
 }

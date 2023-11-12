c87871e212fba72c8845654672729c1cc10096c5
ziadhassan7 <ziadhassan7@gmail.com>
2023-11-12 19:37:46 +0200

    Add: Remove Bookmark Dialog

---
+++ lib/futures/books/presentation/dialog/remove_bookmark/remove_bookmark_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flip_streak/futures/app_common_views/text_view/text_view.dart';
+import 'package:flip_streak/futures/app_common_views/z_dialog/z_dialog.dart';
+import 'package:flip_streak/futures/books/presentation/managers/state_manager/bookmark_list_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RemoveBookmarkDialog {
+
+  final BuildContext context;
+  final WidgetRef ref;
+
+  RemoveBookmarkDialog(this.context, this.ref, String page) {
+    ZDialog(
+      context,
+
+      dominantButtonTitle: "Remove",
+      dominantButtonFunction: () => removeBookmark(page),
+
+      child: const TextView("Remove this page from bookmarks?"),
+    );
+  }
+
+
+  removeBookmark(String page){
+    ref.read(bookmarkListProvider.notifier).removePageFromBookmark(page);
+  }
+
+}
+
+
